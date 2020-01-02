fcd5198dd02aa9546ebf040709b2ae27e60ba39f
Алина <alinascarlett@mail.ru>
2020-01-02 16:24:52 +0300

    Исправляет меню на js

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,6 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 var pageHeader = document.querySelector('.page-header');
-var sectionDownload = document.querySelector('.download');
-var wrapperDownload = document.querySelector('.download__wrapper');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -12,18 +10,10 @@ navToggle.addEventListener('click', function () {
     navMain.classList.add('main-nav--opened');
     pageHeader.classList.add('page-header--opened-menu');
     pageHeader.classList.remove('page-header--closed-menu');
-    sectionDownload.classList.add('download--opened-menu');
-    sectionDownload.classList.remove('download--closed-menu');
-    wrapperDownload.classList.add('download__wrapper--opened-menu');
-    wrapperDownload.classList.remove('download__wrapper--closed-menu');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
     pageHeader.classList.add('page-header--closed-menu');
     pageHeader.classList.remove('page-header--opened-menu');
-    sectionDownload.classList.add('download--closed-menu');
-    sectionDownload.classList.remove('download--opened-menu');
-    wrapperDownload.classList.add('download__wrapper--closed-menu');
-    wrapperDownload.classList.remove('download__wrapper--opened-menu');
   }
 });
